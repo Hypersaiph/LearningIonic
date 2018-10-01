07def62a126f6db32feb36c636776229c3001d4a
Hypersaiph <mygvs.mh@gmail.com>
2018-10-01 16:57:26 -0400

    Greetings App

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,20 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, ToastController} from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
-  constructor(public navCtrl: NavController) {
+  personName: String;
+  constructor(public navCtrl: NavController, private toast: ToastController) {
 
   }
-
+  showGreeting(name: String) {
+    console.log(name);
+    this.toast.create({
+      message: 'Hello, ' + name,
+      duration: 3000
+    }).present();
+  }
 }
