82c7a521bc72d16ff76727381dc0a45e1b8cc03c
geovani-mendoza-mojix-com <mygvs.mh@gmail.com>
2018-10-02 17:26:30 -0400

    F*cking Firebase won't conenct

---
+++ src/app/firebase.credentials.ts
@@ -0,0 +1,8 @@
+export const FIREBASE_CREDENTIALS = {
+  apiKey: "AIzaSyARkVOzDIOnvkJgkD2XqpXeiiA514pBSRA",
+  authDomain: "shopping-app-3a4ac.firebaseapp.com",
+  databaseURL: "https://shopping-app-3a4ac.firebaseio.com",
+  projectId: "shopping-app-3a4ac",
+  storageBucket: "shopping-app-3a4ac.appspot.com",
+  messagingSenderId: "204479597867"
+}
